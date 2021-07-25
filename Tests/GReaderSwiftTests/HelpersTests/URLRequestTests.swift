324a01e75740b1d6f626fee6f6e806d30f9ab175
Thibaut Jarosz <thibaut@jarosz.io>
2021-07-25 15:34:16 +0200

    Rename tests in URLRequestTests

--- Tests/GReaderSwiftTests/HelpersTests/URLRequestTests.swift
+++ Tests/GReaderSwiftTests/HelpersTests/URLRequestTests.swift
@@ -2,18 +2,25 @@ import XCTest
 import Nimble
 
 final class URLRequestTests: XCTestCase {
-    func testHTTPBodyStreamStringWithValidString() {
+    func test_HTTPBodyStreamString_ShouldReturnString_WhenHTTPBodyStreamContainsDataConvertedFromString() {
+        // Given
         let data = "Some string".data(using: .utf8)!
         
+        // When
         var request = URLRequest(url: URL(string: "https://localhost/")!)
         request.httpBodyStream = InputStream(data: data)
         
+        // Then
         expect(request.httpBodyStreamString) == "Some string"
     }
     
-    func testHTTPBodyStreamStringWithEmptyData() {
+    func test_HTTPBodyStreamString_ShouldReturnNil_WhenHTTPBodyStreamDoesNotContainAnyData() {
+        // Given
+        
+        // When
         let request = URLRequest(url: URL(string: "https://localhost/")!)
         
+        // Then
         expect(request.httpBodyStreamString).to(beNil())
     }
 }
