1d0a8195271c3f380877f19335f13a10ebb663a6
Thibaut Jarosz <thibaut@jarosz.io>
2021-09-21 13:56:15 +0200

    Add tag list

--- Sources/GReaderSwift/Models/Tag.swift
+++ Sources/GReaderSwift/Models/Tag.swift
@@ -17,3 +17,35 @@ public extension Tag {
         return String(id.suffix(from: range.upperBound))
     }
 }
+
+public extension Tag {
+    private struct TagContainer: Codable {
+        let tags: [Tag]
+    }
+    
+    static func list(using credentials: Credentials) async throws -> [Tag] {
+        // Create request
+        let request = URLRequest(
+            credentials: credentials,
+            path: .tagList,
+            queryItems: [.jsonOutput]
+        )
+        
+        // Send request
+        let (data, response) = try await URLSession.shared.data(for: request)
+        
+        // Check response status code
+        if let statusCode = (response as? HTTPURLResponse)?.statusCode, statusCode >= 300 {
+            throw GReaderError.serverResponseError(statusCode)
+        }
+        
+        // Parse response by getting first non-empty line
+        do {
+            return try JSONDecoder().decode(TagContainer.self, from: data).tags
+        }
+        catch {
+            // Throw invalidDataResponse if JSON cannot be decoded
+            throw GReaderError.invalidDataResponse(data)
+        }
+    }
+}
--- Sources/GReaderSwift/Models/URLPath.swift
+++ Sources/GReaderSwift/Models/URLPath.swift
@@ -2,5 +2,6 @@ import Foundation
 
 enum URLPath: String {
     case clientLogin = "accounts/ClientLogin"
+    case tagList = "reader/api/0/tag/list"
     case token = "reader/api/0/token"
 }
