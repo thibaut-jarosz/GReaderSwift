9a7a149b65cf2804f80d8181f4414de5e56003bf
Thibaut Jarosz <thibaut@jarosz.io>
2021-09-24 10:47:59 +0200

    Move Tag List extension

--- Sources/GReaderSwift/Models/Tag.swift
+++ Sources/GReaderSwift/Models/Tag.swift
@@ -7,7 +7,7 @@ public struct Tag: Codable, Equatable {
 
 public extension Tag {
     /// Name of the tag if it is a folder inside a `label` subfolder
-    public var name: String? {
+    var name: String? {
         guard
             type == "folder",
             let range = id.range(of: "/label/")
@@ -15,40 +15,5 @@ public extension Tag {
             return nil
         }
         return String(id.suffix(from: range.upperBound))
-    }
-}
-
-public extension Tag {
-    private struct TagContainer: Codable {
-        let tags: [Tag]
-    }
-    
-    /// Retrieve a list of all Tags on the server described in given credentials
-    /// - Parameter credentials: Credentials used to connect to the server
-    /// - Returns: A list of all Tags.
-    public static func list(using credentials: Credentials) async throws -> [Tag] {
-        // Create request
-        let request = URLRequest(
-            credentials: credentials,
-            path: .tagList,
-            queryItems: [.jsonOutput]
-        )
-        
-        // Send request
-        let (data, response) = try await URLSession.shared.data(for: request)
-        
-        // Check response status code
-        if let statusCode = (response as? HTTPURLResponse)?.statusCode, statusCode >= 300 {
-            throw GReaderError.serverResponseError(statusCode)
-        }
-        
-        // Parse response by getting first non-empty line
-        do {
-            return try JSONDecoder().decode(TagContainer.self, from: data).tags
-        }
-        catch {
-            // Throw invalidDataResponse if JSON cannot be decoded
-            throw GReaderError.invalidDataResponse(data)
-        }
     }
 }
