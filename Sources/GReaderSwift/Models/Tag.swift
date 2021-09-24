af07bfe4a9bbf48ff02d40c16d2c8bfa6b73e956
Thibaut Jarosz <thibaut@jarosz.io>
2021-09-24 14:26:10 +0200

    Add internal function to rename tag

--- Sources/GReaderSwift/Models/Tag.swift
+++ Sources/GReaderSwift/Models/Tag.swift
@@ -1,19 +1,36 @@
 import Foundation
 
 public struct Tag: Codable, Equatable {
-    public let id: String
-    public let type: String?
+    public var id: String
+    public var type: String?
 }
 
 public extension Tag {
     /// Name of the tag if it is a folder inside a `label` subfolder
     var name: String? {
+        guard let index = nameStartIndex else {
+            return nil
+        }
+        return String(id.suffix(from: index))
+    }
+    
+    /// Rename the tag if it is a folder inside a `label` subfolder
+    /// - Parameter newName: The new name
+    internal mutating func setName(_ newName: String) throws {
+        guard let index = nameStartIndex else {
+            throw GReaderError.cannotRenameTag
+        }
+        self.id = id.prefix(upTo: index).appending(newName)
+    }
+    
+    /// Starting index of the name
+    private var nameStartIndex: String.Index? {
         guard
             type == "folder",
             let range = id.range(of: "/label/")
         else {
             return nil
         }
-        return String(id.suffix(from: range.upperBound))
+        return range.upperBound
     }
 }
