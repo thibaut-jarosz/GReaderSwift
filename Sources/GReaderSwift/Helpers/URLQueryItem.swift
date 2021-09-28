3a519931bf0d396464208d51764b9c5fbfa24b77
Thibaut Jarosz <thibaut@jarosz.io>
2021-09-28 14:08:18 +0200

    Add a URLQueryItem helper for creating token param

--- Sources/GReaderSwift/Helpers/URLQueryItem.swift
+++ Sources/GReaderSwift/Helpers/URLQueryItem.swift
@@ -1,7 +1,17 @@
 import Foundation
 
 extension URLQueryItem {
+    /// Create a URLQueryItem used to ask for a JSON output
     static var jsonOutput: URLQueryItem {
         .init(name: "output", value: "json")
+    }
+    
+    /// Create a URLQueryItem used to pass credentials' token into a request.
+    /// - Parameter credentials: Credentials used to retrieve the token
+    /// - Returns: A URLQueryItem
+    ///
+    ///  This function can perform a request if the token is not cached yet.
+    static func token(from credentials: Credentials) async throws -> URLQueryItem {
+        await .init(name: "T", value: try credentials.token())
     }
 }
--- Tests/GReaderSwiftTests/Helpers/URLQueryItemTests.swift
+++ Tests/GReaderSwiftTests/Helpers/URLQueryItemTests.swift
@@ -1,4 +1,5 @@
 @testable import GReaderSwift
+import Mocker
 import Nimble
 import XCTest
 
@@ -12,6 +13,40 @@ final class URLQueryItemTests: XCTestCase {
         
         // Then
         expect(result) == URLQueryItem(name: "output", value: "json")
+    }
+    
+    func test_TokenFromCredentials_ShouldUseCachedTokenAndReturnACorrectlyConfiguredQuer() async throws {
+        // Given
+        let credentials = Credentials(
+            baseURL: URL(string: "https://localhost/api/")!,
+            username: "username",
+            authKey: "auth_key"
+        )
+        credentials.cachedTokenTestAccess = "some_cached_token"
+        
+        // When
+        let result = try await URLQueryItem.token(from: credentials)
+        
+        // Then
+        expect(result) == URLQueryItem(name: "T", value: "some_cached_token")
+    }
+    
+    func test_TokenFromCredentials_ShouldMakeATokenRequestOnServerAndReturnACorrectlyConfiguredQueryItem_WhenCachedTokenIsNil() async throws {
+        // Given
+        let baseURL = URL(string: "https://localhost/api/")!
+        let credentials = Credentials(baseURL: baseURL, username: "username", authKey: "auth_key")
+        Mock(
+            url: URL(string: URLPath.token.rawValue, relativeTo: baseURL)!,
+            dataType: .json,
+            statusCode: 200,
+            data: [.get : "some_token".data(using: .utf8)!]
+        ).register()
+        
+        // When
+        let result = try await URLQueryItem.token(from: credentials)
+        
+        // Then
+        expect(result) == URLQueryItem(name: "T", value: "some_token")
     }
     
 }
